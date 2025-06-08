593a40099fe64ed8807ae4132e7d352f345ee2b1
cardosofede <federico.cardoso.e@gmail.com>
2025-06-08 16:24:46 +0700

    (feat) fix mut test

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -36,7 +36,7 @@ async fn subscribe_returns_a_200_for_valid_data() {
     
     let configuration = get_configuration().expect("Failed to read configuration");
     let connection_string = configuration.database.connection_string();
-    let connection = PgConnection::connect(&connection_string)
+    let mut connection = PgConnection::connect(&connection_string)
         .await
         .expect("Failed to connect to Postgres");
     
@@ -54,7 +54,7 @@ async fn subscribe_returns_a_200_for_valid_data() {
     assert_eq!(200, response.status().as_u16(), "Expected a 200 OK response, got: {}", response.status());
     
     let saved = sqlx::query!("SELECT email, name FROM subscriptions")
-        .fetch_one(&connection)
+        .fetch_one(&mut connection)
         .await
         .expect("Failed to fetch saved subscription");
     assert_eq!(saved.email, "ursula_le_guin@gmail.com");
